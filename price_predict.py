c31c70487089222791520ede0105dbe61c83a203
akashmania068 <33367116+akashmania068@users.noreply.github.com>
2017-12-09 11:45:41 +0530

    Loaded Dataset

--- price_predict.py
+++ price_predict.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
+# import timeit
+#
+# time = timeit.timeit(stmt="""pd.read_excel("Dataset/train.xlsx", header=0, usecols=[0, 2, 3, 4, 5, 6, 7])""",
+#                      setup="import pandas as pd",
+#                      number=2)
+# print(time)
+
 dataset = pd.read_excel("Dataset/train.xlsx", header=0, usecols=[0, 2, 3, 4, 5, 6, 7])
 
-x_train = dataset.iloc[:, dataset.columns != 'price']
-y_train = dataset.iloc[:, 5]
+x_train = dataset.iloc[:, dataset.columns != 'price'].values
+y_train = dataset.iloc[:, 5].values
 
+print(x_train)
 print(y_train)
