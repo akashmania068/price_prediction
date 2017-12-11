bf639ef87f1476161d559ea9be7f1764d60109e4
akashmania068 <33367116+akashmania068@users.noreply.github.com>
2017-12-11 21:59:55 +0530

    Reading Pickle file into DataFrame

--- price_predict.py
+++ price_predict.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import Imputer
 
+# If you have not run convert_to_pickle.py, then run it to get the .pkl files
 train_data = pd.read_pickle("train.pkl")
 
 # print(train_data.isnull().sum()) # to get the no. of missing values in each columns
