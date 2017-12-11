f22863f1bb593ce9617619654f8f6360731a37d9
akashmania068 <33367116+akashmania068@users.noreply.github.com>
2017-12-11 12:35:16 +0530

    Loading Dataset

--- price_predict.py
+++ price_predict.py
@@ -1,16 +1,14 @@
 import pandas as pd
 
+## Checking time taken to read the dataset
 # import timeit
 #
 # time = timeit.timeit(stmt="""pd.read_excel("Dataset/train.xlsx", header=0, usecols=[0, 2, 3, 4, 5, 6, 7])""",
 #                      setup="import pandas as pd",
 #                      number=2)
-# print(time)
+# print(time) # takes approx. 110 seconds to read the dataset
 
-dataset = pd.read_excel("Dataset/train.xlsx", header=0, usecols=[0, 2, 3, 4, 5, 6, 7])
+dataset = pd.read_excel(io="Dataset/train.xlsx", header=0)
 
 x_train = dataset.iloc[:, dataset.columns != 'price'].values
 y_train = dataset.iloc[:, 5].values
-
-print(x_train)
-print(y_train)
